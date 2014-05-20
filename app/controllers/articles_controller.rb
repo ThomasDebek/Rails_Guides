a0a46d8865b8123a048805558c090de2036a0e1a
ThomasDebek <tom.debek@gmail.com>
2014-05-20 10:47:17 +0200

    Dodajmy metode index oraz widok tej metody

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
   end
 
@@ -19,5 +23,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+
 
 end
